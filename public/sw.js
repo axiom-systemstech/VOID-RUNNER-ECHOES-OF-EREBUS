c5dfd3f1d655df1a6388bded03493081eb3bd593
Manuel Echepares | AXIOM Systems ◤A-S◢ <echeparesmanuel36@gmail.com>
2026-08-27 23:50:15 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
-sw.js
+const CACHE="void-runner-v1";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match("./index.html"))));
+});
